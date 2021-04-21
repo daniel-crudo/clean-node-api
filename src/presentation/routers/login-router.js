632ffb2093672e5c99ccd9a85f053f162fb158a8
Daniel Crudo <daniel.augusto.silva@accenture.com>
2021-04-21 16:12:27 -0300

    ensure LoginRouter returns 401 when invalid credentials are provided

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -16,5 +16,8 @@ module.exports = class LoginRouter {
         return HttpResponse.badRequest('password')
       }
       this.authUseCase.auth(email, password)
+      return {
+        statusCode: 401
+      }
     }
   }
