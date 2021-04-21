866d4e76a4adb29c2417d8eb54135e5c55a89f6d
Daniel Crudo <daniel.augusto.silva@accenture.com>
2021-04-21 14:02:05 -0300

    ensure LoginRouter reuturns 400 if no password is provided

--- src/presentation/router/login-router.spec.js
+++ src/presentation/router/login-router.spec.js
@@ -1,7 +1,7 @@
 const { TestScheduler } = require("@jest/core")
 class LoginRouter {
   route (httpRequest) {
-    if (!httpRequest.body.email) {
+    if (!httpRequest.body.email || !httpRequest.body.password) {
       return {
         statusCode: 400
       }
@@ -20,4 +20,15 @@ describe('Login Router', () => {
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
   })
+})
+
+test('Should return 400 if no password is provided', () => {
+  const sut = new LoginRouter()
+  const httpRequest = {
+    body: {
+      email: 'any_email@mail.com'
+    }
+  }
+  const httpResponse = sut.route(httpRequest)
+  expect(httpResponse.statusCode).toBe(400)
 })
